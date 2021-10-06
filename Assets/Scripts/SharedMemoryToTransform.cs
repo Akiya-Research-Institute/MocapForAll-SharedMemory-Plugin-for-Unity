d2a18b0351c126f202d21d47fb61534902c50418
Kenji Asaba <kasa@akiya-souken.co.jp>
2021-10-06 13:33:13 +0900

    Initial commit

---
+++ Assets/Scripts/SharedMemoryToTransform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedMemoryToTransform : SharedMemoryReader
+{
+    // output from shared memory
+    public Transform[] data;
+
+    // set as Transform
+    public override void OutputData(float[] buffer)
+    {
+        for (int i = 0; i < data.Length && i * 7 + 6 < buffer.Length; i++)
+        {
+            int offset = i * 7;
+            data[i].rotation = new Quaternion(buffer[offset], buffer[offset + 2], -buffer[offset + 1], buffer[offset + 3]); // (UE4.rot.x, -UE4.rot.z, UE4.rot.y, UE4.rot.w)
+            data[i].position = new Vector3(buffer[offset + 4], buffer[offset + 6], -buffer[offset + 5]) / 100.0f; // (UE4.pos.x, -UE4.pos.z, UE4.pos.y) / 100
+        }
+    }
+}
---
+++ Assets/Scripts/SharedMemoryToVector3.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedMemoryToVector3 : SharedMemoryReader
+{
+    // output from shared memory
+    public Transform[] data;
+
+    // set as Vector3
+    public override void OutputData(float[] buffer)
+    {
+        for (int i = 0; i < data.Length && i * 3 + 2 < buffer.Length; i++)
+        {
+            data[i].position = new Vector3(buffer[i * 3], -buffer[i * 3 + 1], buffer[i * 3 + 2]) / 100.0f; // (ImagePixel.x, -ImagePixel.y, ImagePixel.z) / 100
+        }
+    }
+}
